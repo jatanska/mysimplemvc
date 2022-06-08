59c68ee49c6fdc5dd52e34ee6cea60a0c1702b36
Jarto Tanskanen <jarto.tanskanen@gmail.com>
2022-06-08 13:37:31 +0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/simplemvc/css/my.css"/>
     <style>
-     .container{ padding-top:40px; border: 1px solid #000;}   
+     .container{ margin-top:40px; border: 1px solid #000;}   
     </style>
 </head>
 
